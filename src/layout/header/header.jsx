542371ebc67275ade730de4e961c39bbeb18eddc
MubashirA4 <hamza.cse123@gmail.com>
2024-10-13 22:26:12 +0500

    responsive header

--- src/layout/header/header.jsx
+++ src/layout/header/header.jsx
@@ -1,27 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { BiMessageSquareDetail } from "react-icons/bi";
+import { FaBars } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
 
 const Header = () => {
+
+  const [showMenu, setShowMenu] = useState(false)
+  const [showIcon, setShowIcon] = useState(false)
+
+  const handleMenuClick = () => {
+    setShowMenu(!showMenu);
+    // Toggle showIcon when the menu is opened or closed
+    setShowIcon(showMenu ? false : true);
+  };
   return (
     <div className="container">
       <header>
         <div className="logo">
-          <Link to="/" >MBH</Link>
+          <Link to="/" >MBH<span>.</span></Link>
         </div>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/projects">Projects</Link></li>
-            <li><Link to="/about_me">About</Link></li>
-          </ul>
-        </nav>
-        <div className="logo-msg">
-          <Link to="/contact"><BiMessageSquareDetail className='msg'/></Link>
+        <div className='bar_menu' onClick={handleMenuClick}>
+          {showMenu == true ? <FaTimes /> : <FaBars />}
         </div>
-      </header>
-    </div>
+        <div className={`nav_links ${showMenu == true ? 'show_menu' : 'hide_menu'}`}>
+          <nav >
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/projects">Projects</Link></li>
+              <li><Link to="/about_me">About</Link></li>
+              <div className="logo-msg" >
+                <Link to="/contact">
+                  {showIcon  ? (<span>Get in touch</span>) : (<BiMessageSquareDetail className='msg' />)}
+                </Link>
+              </div>
+            </ul>
+          </nav>
+        </div>
+      </header >
+    </div >
   )
 }
 
--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
 
 const Projects = () => {
   return (
-    <div>
-        <Link to="/form">Form</Link>
-    </div>
+    <Container>
+        <section className='creation'>
+          <h4>My Creations</h4>
+          <p>Explore a decade of my work, showcasing robust and stylish web applications designed to meet diverse needs.</p>
+        </section>
+    </Container>
   )
 }
 
